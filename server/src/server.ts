01e198fd903e9b6c6de96adaea8d3aaa18e598d8
EddyPBR <edvaldojunior1310@gmail.com>
2020-09-29 16:54:30 -0300

    :octocat: Preparando variaveis ambiente

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,10 +2,15 @@ import express from "express";
 import routes from "./routes";
 import cors from "cors";
 
+import "dotenv/config";
+
 const app = express();
+
+const PORT = process.env.PORT;
+const HOST = process.env.HOST;
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3334);
+app.listen(PORT, () => console.log(`MURAL DIGITAL > Servidor online em: http://${HOST}:${PORT}/`));
