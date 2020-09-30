ee33200d1077ed7e67dff645de18b8c2f7fad0ba
EddyPBR <edvaldojunior1310@gmail.com>
2020-09-30 14:43:36 -0300

    :package: Adicionando rota de autenticaçao criptografada com JWT

---
+++ server/src/controllers/AuthController.ts
@@ -0,0 +1,33 @@
+import { Request, Response } from "express";
+import { getRepository } from "typeorm";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+import "dotenv/config";
+
+import User from "@models/UserModel";
+
+class AuthController {
+  async authenticate(request: Request, response: Response) {
+    const repository = getRepository(User);
+    const { email, password } = request.body;
+
+    const user = await repository.findOne({ where: { email } });
+    if(!user) return response.sendStatus(401);
+
+    const isValidPassword = await bcrypt.compare(password, user.password);
+    if (!isValidPassword) return response.sendStatus(401);
+
+    const secret = process.env.JWT_SECRET as string;
+    const token = jwt.sign({ id: user.id }, secret, { expiresIn: "1d" });
+
+    delete user.password;
+
+    return response.json({
+      user,
+      token
+    });
+  }
+}
+
+export default AuthController;
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,11 +1,14 @@
 import express from "express";
 
 import UserController from "@controllers/UserController";
+import AuthController from "@controllers/AuthController";
 
 const userController = new UserController();
+const authController = new AuthController();
 
 const routes = express.Router();
 
 routes.post("/users", userController.create);
+routes.post("/auth", authController.authenticate);
 
 export default routes;
