f2ede56605938c8888006cc0644bbd94dd8ec5e5
EddyPBR <edvaldojunior1310@gmail.com>
2020-10-01 16:09:13 -0300

    :package: Criando rota de cadastro e listagem de billboards

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -4,9 +4,11 @@ import authMiddleware from "./middlewares/authMiddleware";
 
 import UserController from "@controllers/UserController";
 import AuthController from "@controllers/AuthController";
+import Billboard from "@controllers/BillboardController";
 
 const userController = new UserController();
 const authController = new AuthController();
+const billboard = new Billboard();
 
 const routes = express.Router();
 
@@ -14,5 +16,8 @@ routes.get("/users", authMiddleware, userController.index);
 routes.post("/users", userController.create);
 
 routes.post("/auth", authController.authenticate);
+
+routes.get("/billboard", billboard.index);
+routes.post("/billboard", billboard.create);
 
 export default routes;
