ee692c162a78898c4b40c5f574411686964981ed
EddyPBR <edvaldojunior1310@gmail.com>
2020-10-06 10:29:31 -0300

    :package: Preparando estrutura para styled-components

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
+import { GlobalStyle } from "./assets/styles/GlobalStyle";
 import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>,
   document.getElementById("root")
