ca50a658b46c2df63c703900d63f55c2efa9eaea
EddyPBR <edvaldojunior1310@gmail.com>
2020-10-07 10:14:08 -0300

    :package: Criando component Footer e adicionando nas rotas

---
+++ web/src/components/Footer/index.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import styled from "styled-components";
+
+const Footer: React.FC = () => {
+  return (
+    <FooterContent>
+      <span>Desenvolvido por: </span>
+      <a href="https://www.linkedin.com/in/edvaldojuniordev/" target="_blank" rel="noopener noreferrer">Edvaldo Junior</a>
+    </FooterContent>
+  );
+};
+
+const FooterContent = styled.div`
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  height: 3rem;
+  background: linear-gradient(92.57deg, #2F2E41 0%, #3F3D56 100%);
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+
+  && > span {
+    font: 400 1.2rem "Roboto", sans-serif;
+    color: var(--color-background);
+  }
+
+  && > a {
+    font: 700 1.2rem "Open Sans", sans-serif;
+    color: var(--color-background);
+    margin-left: .5rem;
+    text-decoration: none;
+    transition: .3s color;
+  }
+
+  && > a:hover {
+    color: var(--color-primary);
+  }
+
+`;
+
+export default Footer;
--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -3,11 +3,15 @@ import { BrowserRouter, Route } from "react-router-dom";
 
 import Mural from "./pages/Mural";
 
+import Footer from "./components/Footer";
+
 const Routes: React.FC = () => {
   return (
     <BrowserRouter>
       <Route path="/" exact component={Mural} />
+      <Footer />
     </BrowserRouter>
+    
   );
 };
 
