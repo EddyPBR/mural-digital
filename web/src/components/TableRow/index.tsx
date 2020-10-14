1838e04e5c2949a09da3310f6f078f7ebb5b7751
EddyPBR <edvaldojunior1310@gmail.com>
2020-10-14 09:35:25 -0300

    :wrench: removendo component DeleteBox | Código foi transferido ao component tablerow

--- web/src/components/TableRow/index.tsx
+++ web/src/components/TableRow/index.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
+
 import { FaTrash, FaEdit } from "react-icons/fa";
 
 interface TableRow {
@@ -11,6 +12,24 @@ interface TableRow {
 
 const TableRow: React.FC<TableRow> = (props) => {
   const { id, title, date } = props;
+
+  const [enableDelete, setEnableDelete] = useState(false);
+
+  const handleShowDeleteBox = (event: any) => {
+    event.preventDefault();
+    setEnableDelete(true);
+  };
+
+  const handleHideDeleteBox = (event: any) => {
+    event.preventDefault();
+    setEnableDelete(false);
+  }
+
+  const handleDeleteAnnounce = (event: any) => {
+    event.preventDefault();
+    alert("need create a api call");
+  }
+
   return (
     <Row>
       <td>{id}</td>
@@ -22,13 +41,31 @@ const TableRow: React.FC<TableRow> = (props) => {
         </Link>
       </td>
       <td>
-        <Link onClick={ (event) => {
-          event.preventDefault();
-          console.log("ok")
-        } } to={`/announces/delete/${id}`}>
+        <Link
+          onClick={(event) => handleShowDeleteBox(event)}
+          to={`/announces/delete/${id}`}
+        >
           <FaTrash size={18} />
         </Link>
       </td>
+      {enableDelete && (
+        <BlackWindow>
+          <Box>
+            <Title>Desejar remover este anúncio?</Title>
+            <Info>
+              O anúncio <span>{`${title}`}</span> ao clicar em confirmar, o
+              anúncio será removido do sistema e jamais poderá ser restaurado,
+              deseja confirmar a remoção?
+            </Info>
+            <RowBox>
+              <Button>
+                <Link onClick={(event) => handleDeleteAnnounce(event)} to={`/admin/announces/delete/${id}`}>REMOVER</Link>
+              </Button>
+              <Link onClick={(event) => handleHideDeleteBox(event)} to={`/admin/announces/`}>Não remover</Link>
+            </RowBox>
+          </Box>
+        </BlackWindow>
+      )}
     </Row>
   );
 };
@@ -107,6 +144,141 @@ const Row = styled.tr`
     border-color: rgba(213, 21, 26, 0.2);
     transition: 0.3s;
     cursor: pointer;
+  }
+`;
+
+// Delete Box
+const BlackWindow = styled.td`
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  z-index: 1000;
+  background-color: rgba(0, 0, 0, 0.7);
+
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Box = styled.div`
+  width: 90vw;
+  max-width: 43rem;
+  min-height: 38rem;
+  background: #ffffff;
+  box-shadow: 2px 2px 4px 2px rgba(0, 0, 0, 0.25);
+  border-radius: 10px;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: flex-start;
+  padding: 3rem 4rem;
+
+  @media (max-width: 480px) {
+    padding: 3rem 2rem;
+  }
+`;
+
+const Title = styled.h1`
+  font: 400 3.6rem/4.2rem "Roboto", sans-serif;
+  color: var(--color-primary);
+  margin: 0;
+  margin-bottom: 2rem;
+`;
+
+const Info = styled.p`
+  font: 400 1.6rem/2.6rem "Roboto", sans-serif;
+  color: var(--color-text);
+  margin-bottom: 4rem;
+
+  && > span {
+    font: 700 1.6rem "Open Sans", sans-serif;
+    color: var(--color-title);
+  }
+`;
+
+const RowBox = styled.div`
+  width: 100%;
+  margin-bottom: 2rem;
+
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+
+  -webkit-touch-callout: none;
+  -webkit-user-select: none;
+  -khtml-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+
+  && > a {
+    color: var(--color-primary);
+    font: 400 1.6rem "Roboto", sans-serif;
+    text-decoration: none;
+    margin-left: 3rem;
+  }
+
+  && > a:hover {
+    text-decoration: underline;
+  }
+`;
+
+const Button = styled.div`
+  width: 90vw;
+  max-width: 17rem;
+  height: 5rem;
+  background: linear-gradient(
+    94.19deg,
+    var(--color-primary-light) 0%,
+    var(--color-primary-dark) 100%
+  );
+  box-shadow: 4px 4px 5px rgba(0, 0, 0, 0.5);
+  border-radius: 10px;
+  transition: 0.2s filter, 0.2s box-shadow;
+  cursor: pointer;
+  -webkit-touch-callout: none;
+  -webkit-user-select: none;
+  -khtml-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+
+  &&:hover {
+    filter: brightness(1.1);
+    box-shadow: 4px 4px 8px rgba(0, 0, 0, 0.8);
+  }
+
+  && > a {
+    width: 100%;
+    height: 100%;
+    text-decoration: none;
+    font: 700 1.6rem "Open Sans", sans-serif;
+    color: var(--color-background);
+
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+  }
+
+  && > a > svg {
+    margin-right: 0.8rem;
+  }
+
+  @media (max-width: 520px) {
+    width: 14rem;
+    height: 4.6rem;
+    font-size: 1.6rem;
   }
 `;
 
