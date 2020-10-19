1680b96b9a07103b2d17cc0b6ae5db0fdefd306b
EddyPBR <edvaldojunior1310@gmail.com>
2020-10-19 14:40:09 -0300

    :wrench: isLogin agora utiliza a rota de verificacao de autenticacao da api

--- web/src/utils/Login.ts
+++ web/src/utils/Login.ts
@@ -1,4 +1,6 @@
-const TOKEN_KEY = "acess_token";
+import api from "../services/api";
+
+const TOKEN_KEY = "access_token";
 
 export const login = (token: string) => {
   sessionStorage.setItem(TOKEN_KEY, token);
@@ -9,9 +11,16 @@ export const logout = () => {
 };
 
 export const isLogin = () => {
-  if (sessionStorage.getItem(TOKEN_KEY)) {
-    return true;
-  }
+  if (!sessionStorage.getItem(TOKEN_KEY)) return false;
 
-  return false;
+  const authorization = api
+    .get("/auth", {
+      headers: {
+        Authorization: `Bearer ${sessionStorage.getItem("access_token")}`,
+      },
+    })
+    .then((response) => true)
+    .catch((error) => false);
+
+  return authorization;
 };
