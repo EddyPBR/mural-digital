7e758965d7d858bfd96ebfa7440de274b488b8dc
EddyPBR <edvaldojunior1310@gmail.com>
2020-10-26 10:35:45 -0300

    :wrench: Alterando estilos da tela de anuncios

--- mobile/src/screens/Announce/index.tsx
+++ mobile/src/screens/Announce/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { ScrollView } from "react-native";
 import styled from "styled-components/native";
 
 import Example from "../../assets/images/boilerboard.png";
@@ -23,41 +24,40 @@ const Announce: React.FC = () => {
   );
 };
 
-const Screen = styled.View`
+const Screen = styled.ScrollView`
   display: flex;
   flex-direction: column;
-  justify-content: flex-start;
-  align-items: center;
   flex: 1;
   padding: 32px 32px;
+  margin-bottom: 32px;
 `;
 
 const Image = styled.Image`
   margin: 12px 0px;
   max-width: 224px;
   max-height: 224px;
+  align-self: center;
 `;
 
 const Date = styled.Text`
   font: 700 14px "OpenSans";
-  text-align: right;
+  text-align: left;
   color: #253137;
-  align-self: flex-start;
-  margin-top: 16px;
+  margin-top: 8px;
+  margin-bottom: 2px;
 `;
 
 const Title = styled.Text`
-  font: 700 24px "OpenSans";
-  margin: 16px 0px;
+  font: 700 32px "OpenSans";
   color: #253137;
+  margin-bottom: 24px;
 `;
 
 const Text = styled.Text`
-  font: 400 14px "OpenSans";
-  line-height: 22px;
+  font: 400 16px "OpenSans";
+  line-height: 26px;
   color: #253137;
-  height: 56px;
-  margin-bottom: 12px;
+  margin-bottom: 16px;
 `;
 
 export default Announce;
