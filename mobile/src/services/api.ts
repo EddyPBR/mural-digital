b67ccc5265d2d1a4276e442d800c1079ad509ebb
EddyPBRm <edvaldojunior1310@gmail.com>
2020-11-19 15:42:32 -0300

    :wrench: Modificando arquivo de chamada a api

--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -1,7 +1,10 @@
 import axios from "axios";
 
+const HOST = "192.168.15.126"
+const PORT = "3001"
+
 const api = axios.create({
-  baseURL: "http://192.168.1.211:3001",
+  baseURL: `http://${HOST}:${PORT}`,
 });
 
 export default api;
